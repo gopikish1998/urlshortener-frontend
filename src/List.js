4381fa4c5756dbaa33182fb0da912deb6c6138ca
Gopi Kishore <kishorepenjendra@gmail.com>
2022-01-01 13:37:29 +0530

    delete

--- src/List.js
+++ src/List.js
@@ -18,6 +18,13 @@ function List() {
         else{await fetchUrls()
         console.log(url)}
     }, [])
+    let handleDelete = async(e,_id)=>{
+      e.preventDefault();
+      await axios.delete(`${env.api}/delete/${_id}`,{headers:{
+        "Authorization":window.localStorage.getItem("url_login")
+    }})
+    fetchUrls()
+    }
     return (
         <div className='container d-flex justify-content-center align-items-center'>
             <table class="table table-dark table-responsive" width="100%" cellspacing="0">
@@ -27,6 +34,7 @@ function List() {
       <th scope="col">URL</th>
       <th scope="col">Short URL</th>
       <th scope="col">Clicks</th>
+      <th scope='col'>Action</th>
     </tr>
   </thead>
   <tbody>
@@ -36,6 +44,7 @@ function List() {
           <td><a target="_blank" href={obj.url}>{obj.url}</a></td>
           <td><a target="_blank" href={`${env.api}/${obj.short}`}>{obj.short}</a></td>
           <td>{obj.clicks||0}</td>
+          <td><button className='btn btn-outline-danger' onClick={e=>handleDelete(e,obj._id)}>DELETE</button></td>
         </tr>
       )})}
   </tbody>
